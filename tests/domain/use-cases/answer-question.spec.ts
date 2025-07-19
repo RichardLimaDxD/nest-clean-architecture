e469b91d07b87f4cdf1b693f5f70fe419dbeb94f
RichardLimaDxD <richardpires663802@gmail.com>
2025-07-19 15:06:32 -0300

    feat: answer repository - tests

--- tests/domain/use-cases/answer-question.spec.ts
+++ tests/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,18 @@
 import { expect, test } from "vitest";
 import { AnswerQuestionUseCase } from "../../../src/domain/use-cases/answer-question";
+import { AnswerRepository } from "../../../src/domain/repositories/answer-repository";
+import { Answer } from "../../../src/domain/entities/answer";
 
-test("create an answer", () => {
-  const answerQuestion = new AnswerQuestionUseCase();
+const fakeAnswersRepository: AnswerRepository = {
+  create: async (answer: Answer) => {
+    return;
+  },
+};
 
-  const answer = answerQuestion.execute({
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
+
+  const answer = await answerQuestion.execute({
     questionId: "1",
     instructorId: "1",
     content: "New response",
